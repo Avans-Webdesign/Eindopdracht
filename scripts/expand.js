1a286d78ccfaeb5e1373d736376d542ef2c367b0
Somfic <somfic@gmail.com>
2019-10-12 12:33:48 +0200

    Update doc selectie

--- scripts/expand.js
+++ scripts/expand.js
@@ -1,3 +1,23 @@
-function ShowDoc(id) {
-    $("#doc").load("../content/" + id + ".html");
+var openTijd = 3000;
+var sluitTijd = 1000;
+
+function ShowDoc(id, text) {
+    $("#doc, #doc-title").animate({
+        opacity: 0
+    }, {
+        "duration": sluitTijd,
+        "queue": false
+    });
+    $("#doc").slideToggle(sluitTijd, function() {
+        $("#doc-title").text(text);
+        $("#doc, #doc-title").animate({
+            opacity: 1
+        }, {
+            "duration": sluitTijd,
+            "queue": false
+        });
+        $("#doc").load("../content/" + id + ".html", function() {
+            $("#doc").slideToggle(openTijd);
+        });
+    });
 }
